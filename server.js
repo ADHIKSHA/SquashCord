97e3d79e90ccf8476dae9e6f495562b03955b0ac
ADHIKSHA <adhikshat1905@gmail.com>
2021-05-23 18:51:26 +0530

    PORT changes

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ var httpServer = http.createServer(app);
 var server = https.createServer(credentials, app);
 
 httpServer.listen(8080);
-//server.listen(8443);
+server.listen(3000);
 
 const io = socketio(server);
 
@@ -98,6 +98,6 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 8443;
+//const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
